d4d6e04fed36eaf564c6b71585cfcb97d6833bf6
Xavier Chico <xavierchico@Xaviers-MacBook-Pro.local>
2021-12-06 18:14:23 -0800

    Second Push

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,60 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 // First, tell us your name
 let yourName = "Xavier Chico" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-
-
+let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
-
-
 let sugar = 0   // Sugar Sprinkle
-document.getElementById('qty-gb')
+let total = 0   // Total Cookie
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
+
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
-    gb++
-
-    document.getElementById('qty-gb').textContent=gb
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    gb++;
+    document.getElementById('qty-gb').textContent = gb;
+    total++;
+    document.getElementById('qty-total').textContent = total;
+  })
+document.getElementById('minus-gb').addEventListener('click', function() {
+    gb--;
+    document.getElementById('qty-gb').textContent= gb;
+    total--;
+    document.getElementById('qty-total').textContent = total;
 })
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-cc').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
-    cc++
 
-    document.getElementById('qty-cc').textContent=cc
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})// Event listener for clicks on the "+" button for Gingerbread cookies
+
+
+
+document.getElementById('add-cc').addEventListener('click', function(){
+  cc++;
+  document.getElementById('qty-cc').textContent = cc;
+  total++;
+  document.getElementById('qty-total').textContent = total;
+})
+
+document.getElementById('minus-cc').addEventListener('click', function(){
+  cc--;
+  document.getElementById('qty-cc').textContent = cc;
+  total--;
+  document.getElementById('qty-total').textContent= total;
+})
+
+
+
+
+
 document.getElementById('add-sugar').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
-    sugar++
-
-    document.getElementById('qty-sugar').textContent=sugar
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+  sugar++;
+  document.getElementById('qty-sugar').textContent = sugar;
+  total++;
+  document.getElementById('qty-total').textContent = total;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('minus-sugar').addEventListener('click', function() {
+  sugar--;
+  document.getElementById('qty-sugar').textContent = sugar;
+  total--;
+  document.getElementById('qty-total').textContent = total;
+  })
